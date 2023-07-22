be3ba6e6189e55ae50eadfa06fbc720d72d0e7aa
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-07-23 00:48:34 +0100

    Updated useIPFS file

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -1,8 +1,8 @@
  const useIPFS = () => {
     const resolveLink = (url) => {
         if(!url) return;
-        if (url.includes("https://ipfs.io/ipfs")) return url;
-        return url.replace("https://infura-ipfs.io/ipfs", "https://ipfs.io/ipfs");
+        if (url.includes("https://spotify-web3.infura-ipfs.io/ipfs")) return url;
+        return url.replace("https://infura-ipfs.io/ipfs", "https://spotify-web3.infura-ipfs.io/ipfs");
     }
     return {resolveLink}
 }
